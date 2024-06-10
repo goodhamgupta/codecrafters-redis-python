137838232aa4d276cd82f9191b47771f36bdc4bf
goodhamgupta <shubhamg2208@live.com>
2024-06-10 23:25:53 +0800

    Add support for GET and SET commands

--- app/main.py
+++ app/main.py
@@ -11,6 +11,21 @@ PARAM_IDX = 4
 
 
 class Parser:
+    db = {}
+
+    @staticmethod
+    def _extract_content(cmd_list, content_len_idx, content_idx):
+        if len(cmd_list) > content_idx:
+            param_len = int(cmd_list[content_len_idx][1:])
+            param_content = cmd_list[content_idx]
+            assert (
+                len(param_content) == param_len
+            ), f"Expected parameter of length {param_len} but received {len(param_content)}"
+            return param_len, param_content
+            return f"${param_len}\r\n{param_content}\r\n".encode("utf-8")
+        else:
+            raise Exception("Invalid command format for ECHO")
+
     @classmethod
     def parse_command(cls, cmd_list):
         _num_elements = int(cmd_list[ARGS_IDX][1:])
@@ -23,15 +38,29 @@ class Parser:
         if cmd == "PING":
             return b"+PONG\r\n"
         elif cmd == "ECHO":
-            if len(cmd_list) > PARAM_IDX:
-                param_len = int(cmd_list[PARAM_LEN_IDX][1:])
-                param_content = cmd_list[PARAM_IDX]
-                assert (
-                    len(param_content) == param_len
-                ), f"Expected parameter of length {param_len} but received {len(param_content)}"
-                return f"${param_len}\r\n{param_content}\r\n".encode("utf-8")
+            param_len, param_content = cls._extract_content(
+                cmd_list, PARAM_LEN_IDX, PARAM_IDX
+            )
+            return f"${param_len}\r\n{param_content}\r\n".encode("utf-8")
+        elif cmd == "SET":
+            _key_len, key_content = cls._extract_content(
+                cmd_list, PARAM_LEN_IDX, PARAM_IDX
+            )
+            _val_len, val_content = cls._extract_content(
+                cmd_list, PARAM_LEN_IDX + 2, PARAM_IDX + 2
+            )
+            cls.db.update({key_content: val_content})
+            print("DB: ", cls.db)
+            return b"+OK\r\n"
+        elif cmd == "GET":
+            _key_len, key_content = cls._extract_content(
+                cmd_list, PARAM_LEN_IDX, PARAM_IDX
+            )
+            if key_content in cls.db:
+                value = cls.db[key_content]
+                return f"${len(value)}\r\n{value}\r\n".encode("utf-8")
             else:
-                raise Exception("Invalid command format for ECHO")
+                return b"$-1\r\n"
         else:
             raise Exception(f"Command {cmd} not supported!")
 
