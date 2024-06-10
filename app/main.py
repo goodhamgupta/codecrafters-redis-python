8150a11e364f7c420c32ded9e09cfad362f3a170
goodhamgupta <shubhamg2208@live.com>
2024-06-10 20:46:02 +0800

    Add support for multiple command

--- app/main.py
+++ app/main.py
@@ -2,13 +2,16 @@ import socket
 
 
 MAX_NUM_UNACCEPTED_CONN = 10
+
+
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     server_socket.listen(MAX_NUM_UNACCEPTED_CONN)
     while server_socket:
-        (client_socket, client_add) = server_socket.accept()
-        _data = client_socket.recv(1024).decode("utf-8")
-        client_socket.sendall(b"+PONG\r\n")
+        (client_socket, _client_add) = server_socket.accept()
+        data = client_socket.recv(1024).decode("utf-8")
+        for _cmd in data.split("\n"):
+            client_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
