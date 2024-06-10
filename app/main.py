c35da4d84aff865abc83529de6624fb95e8b55f2
goodhamgupta <shubhamg2208@live.com>
2024-06-10 20:42:25 +0800

    Add support for PING

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 import socket
 
 
+MAX_NUM_UNACCEPTED_CONN = 10
 def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()
+    server_socket.listen(MAX_NUM_UNACCEPTED_CONN)
+    while server_socket:
+        (client_socket, client_add) = server_socket.accept()
+        _data = client_socket.recv(1024).decode("utf-8")
+        client_socket.sendall(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
